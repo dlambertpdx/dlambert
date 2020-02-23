b0fc91ec5de06dc6567b2d8f654bb762c9f010f1
sanvean74 <dlambertpdx@gmail.com>
2020-02-22 17:23:26 -0800

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.css';
+
+export default function App() {
+  return (
+    <section className={styles.Parallax}>
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+      <div id='stars4'></div>
+      <h1>Donna Lambert</h1>
+    </section>
+  );
+}
